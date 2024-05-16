c510dc57098248e7c34c6133c0f5d62afba42a53
Beni Samuel <benisamuel566@gmail.com>
2024-05-16 11:15:32 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// menu functionalities
+
+let menu=document.getElementById("menu");
+menu.addEventListener("click",()=>{
+    let divNavUls=document.createElement("div");
+    divNavUls.style={
+       background:red
+    }
+})
